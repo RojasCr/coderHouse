b4718c74b462adc4de532816a4ce962d451cac04
Cristian Rojas <cristianrojas781@gmail.com>
2023-01-12 10:47:22 -0300

    avance1

--- Clase3/entregable/productManager.js
+++ Clase3/entregable/productManager.js
@@ -12,45 +12,59 @@ class ProductManager {
     
     
     //Método para agregar productos
-    addProduct(title, description, price, thumbail, code, stock){
-        /*try{*/
+    async addProduct(title, description, price, thumbail, code, stock){
+        this.id = this.id + 1;
+        
+        const producto = {
+            id: this.id,
+            title,
+            description,
+            price,
+            thumbail,
+            code,
+            stock
+        }
+        try{
 
-            this.id +=1
-            
-            const producto = {
-                id: this.id,
-                title,
-                description,
-                price,
-                thumbail,
-                code,
-                stock
-            }
             
             //Validaciones
-            /*let resultado = await fs.promises.readFile(this.path, this.type);
-            let resultadoObj = JSON.parse(resultado);
-    
-            let repetido = await resultadoObj.find( c => c.code == code);
-            if(repetido){
-                return `El code ya existe`;
+            if(!fs.existsSync(this.path)){
+                
+                let productsStr = JSON.stringify(this.products, null, " ");
+                await fs.promises.writeFile(this.path, productsStr);
+            } 
+            if(fs.existsSync(this.path)){
+
+                let resultado = await fs.promises.readFile(this.path, this.type);
+                //let resultadoStr1 = resultado;
+                let resultadoObj = JSON.parse(resultado);
+        
+                let repetido = resultadoObj.find( c => c.code == code);
+                if(repetido){
+                    console.log(`El code ya existe`);
+                    return;
+                }
+                if(!title || !description || !price || !thumbail || !code || !stock){
+                    console.log(`Faltan datos`);
+                    return;
+                }
+        
+                //Agregar producto a products
+                resultadoObj.push(producto);
+                
+                
+                //Guardarlo en archivo json
+                let resultadoStr2 = JSON.stringify(resultadoObj, null, " ");
+                await fs.promises.writeFile(this.path, resultadoStr2);
+        
+        
+                console.log(`Producto agregado con id: ${this.id}`);
+                
             }
-            if(!title || !description || !price || !thumbail || !code || !stock){
-                return `Faltan datos`;
-            }*/
-    
-            //Agregar producto a products
-            this.products.push(producto);
-            
-            //Guardarlo en archivo json
-            /*let productsStr = JSON.stringify(this.products);
-            await fs.promises.appendFile(this.path, productsStr);
-    */
-    
-            //return `Producto agregado con id: ${this.id}`;
-        /*} catch (err){
+        } catch (err){
+            //console.log(err)
             throw new Error(err);
-        }*/
+        }
 
         
     }
@@ -92,7 +106,10 @@ class ProductManager {
 
 const manejadorDeProductos = new ProductManager();
 
-//console.log(manejadorDeProductos.addProduct("agua", "bebida", 100, "url", "code", 1000));
-//console.log(manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code", 9000));
+manejadorDeProductos.addProduct("agua", "bebida", 100, "url", "code2712", 1000);
+manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code6879", 9000);
+manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code68797", 9000);
+//manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code145", 9000);
+//manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code14578", 9000);
 //manejadorDeProductos.getProducts();
-manejadorDeProductos.getProductById(1);
+//manejadorDeProductos.getProductById(1);
