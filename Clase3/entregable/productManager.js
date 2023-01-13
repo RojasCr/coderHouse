9e58ba62c8b43873a063c18c7f289681c9238747
Cristian Rojas <cristianrojas781@gmail.com>
2023-01-13 11:22:05 -0300

    avance2

--- Clase3/entregable/productManager.js
+++ Clase3/entregable/productManager.js
@@ -4,7 +4,7 @@ class ProductManager {
     products = [];
     
     constructor(){
-        this.id = 0;
+        //this.id = 0;
         this.path = "./files/products.json";
         this.type = "utf-8";
     }
@@ -13,10 +13,10 @@ class ProductManager {
     
     //Método para agregar productos
     async addProduct(title, description, price, thumbail, code, stock){
-        this.id = this.id + 1;
+        //this.id = this.id + 1;
         
-        const producto = {
-            id: this.id,
+        let producto = {
+            id: 0,
             title,
             description,
             price,
@@ -24,6 +24,7 @@ class ProductManager {
             code,
             stock
         }
+
         try{
 
             
@@ -48,24 +49,29 @@ class ProductManager {
                     console.log(`Faltan datos`);
                     return;
                 }
-        
+                
                 //Agregar producto a products
+                if(resultadoObj.length == 0){
+                    producto.id = 1
+                } else {
+                    producto.id = resultadoObj[resultadoObj.length - 1].id + 1;
+                }
                 resultadoObj.push(producto);
                 
                 
                 //Guardarlo en archivo json
                 let resultadoStr2 = JSON.stringify(resultadoObj, null, " ");
                 await fs.promises.writeFile(this.path, resultadoStr2);
-        
-        
-                console.log(`Producto agregado con id: ${this.id}`);
                 
+                
+                console.log(`Producto agregado con id: ${producto.id}`);
+                return;
             }
         } catch (err){
             //console.log(err)
             throw new Error(err);
         }
-
+        
         
     }
     
@@ -77,7 +83,7 @@ class ProductManager {
             const resultadoObj = JSON.parse(resultadoStr);
             console.log(resultadoObj);
         } catch (err) {
-                throw new Error(err);
+            throw new Error(err);
             
         }
     }
@@ -89,27 +95,101 @@ class ProductManager {
             let resultadoObj = await JSON.parse(resultado);
             let producto = resultadoObj.find( p => 
                 p.id == i
-            );
+                );
     
+                if(producto){
+                    console.log(producto);
+                    return producto;
+                } else {
+                    console.log(`Not found`);
+                    return `Not found`;
+                }
+        } catch (err){
+                throw new Error(err);
+        }
+    }
+        
+    async updateProduct(i, title, description, price, thumbail, code, stock){
+        try{
+            let resultado = await fs.promises.readFile(this.path, this.type);
+            let resultadoObj = await JSON.parse(resultado);
+            
+            let repetido = resultadoObj.find( c => c.code == code);
+            if(repetido){
+                console.log(`El code ya existe`);
+                return;
+            }
+            
+            let producto = resultadoObj.find( p => 
+                p.id == i
+            );
+                    
+            let productoActualizado = {
+                id: i,
+                title,
+                description,
+                price,
+                thumbail,
+                code,
+                stock
+            };
+                    
             if(producto){
                 console.log(producto);
+                let productoIndice = resultadoObj.indexOf(producto);
+                console.log(productoIndice);
+                resultadoObj[productoIndice] = productoActualizado;
+                console.log(resultadoObj);
+                //Guardarlo en archivo json
+                let actualizadoStr = JSON.stringify(resultadoObj, null, " ");
+                await fs.promises.writeFile(this.path, actualizadoStr);
+                return;
             } else {
                 console.log(`Not found`);
+                //return `Not found`;
             }
-        } catch{ (err) => {
-
+        } catch (err){
             throw new Error(err);
         }
+    }
+    
+    async deleteProduct(i){
+        try{
+            let resultado = await fs.promises.readFile(this.path, this.type);
+            let resultadoObj = await JSON.parse(resultado);
+            let producto = resultadoObj.find( p => 
+                p.id == i
+            );
+        
+            if(producto){
+                console.log(producto);
+                let productoIndice = resultadoObj.indexOf(producto);
+                console.log(productoIndice);
+                resultadoObj.splice(productoIndice, 1);
+                console.log(resultadoObj);
+                
+                //Guardarlo en archivo json
+                let actualizadoStr = JSON.stringify(resultadoObj, null, " ");
+                await fs.promises.writeFile(this.path, actualizadoStr);
+                return;
+            } else {
+                console.log(`Not found`);
+                return `Not found`;
+            }
+        } catch (err){
+            throw new Error(err);
         }
     }
 }
 
 const manejadorDeProductos = new ProductManager();
 
-manejadorDeProductos.addProduct("agua", "bebida", 100, "url", "code2712", 1000);
-manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code6879", 9000);
-manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code68797", 9000);
+//manejadorDeProductos.addProduct("agua", "bebida", 100, "url", "code2712", 1000);
+//manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code6879", 9000);
+//manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code68797", 9000);
 //manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code145", 9000);
 //manejadorDeProductos.addProduct("coca", "bebida", 500, "url1", "code14578", 9000);
 //manejadorDeProductos.getProducts();
-//manejadorDeProductos.getProductById(1);
+//manejadorDeProductos.getProductById(2);
+manejadorDeProductos.updateProduct(3, "manaos", "bebida", 5500, "url1", "code1578", 9000);
+//manejadorDeProductos.deleteProduct(4);
